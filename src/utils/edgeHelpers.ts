e3e9ee57c68095bcf102299a65ff98328af9169b
thundermiracle <cissoliu@gmail.com>
2022-07-30 16:19:15 +0900

    feat: analyze search parameters

--- src/utils/edgeHelpers.ts
+++ src/utils/edgeHelpers.ts
@@ -1,3 +1,4 @@
+import dayjs from 'dayjs';
 import { NextResponse } from 'next/server';
 
 export function createCacheResponse(data: { [key: string | number]: any }) {
@@ -8,4 +9,52 @@ export function createCacheResponse(data: { [key: string | number]: any }) {
       'Cache-Control': 'max-age=0, s-maxage=86400',
     },
   });
+}
+
+export function createErrorResponse(errorMsg = '') {
+  return NextResponse.json(
+    { error: errorMsg },
+    {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+}
+
+export function getQueryParams(
+  url: string,
+  queryParams: string[],
+): (string | null)[] {
+  const query = new URLSearchParams(url);
+
+  return queryParams.map((key) => query.get(key));
+}
+
+export function getFromTo(url: string): {
+  error?: string;
+  data?: { from: dayjs.Dayjs; to: dayjs.Dayjs };
+} {
+  const [from, to] = getQueryParams(url, ['from', 'to']);
+  if (from == null) {
+    return {
+      error: 'Missing "from" query parameter',
+    };
+  }
+
+  // format
+  const data = { from: dayjs(from), to: dayjs(to) };
+
+  // validate
+  if (!data.from.isValid() || !data.to.isValid()) {
+    return { error: 'query parameter "from" or "to" is not a valid date' };
+  }
+
+  // validate range
+  if (to == null || dayjs(to).isBefore(dayjs(from))) {
+    data.to = dayjs(from).add(14, 'day');
+  }
+
+  return { data };
 }
