afd388ee3cb83f15e3b411e6e2282dce263941f1
ThunderMiracle <cissoliu@gmail.com>
2022-12-25 16:18:36 +0900

    feat: add serverless function version of holidays endpoint (#49)

---
+++ src/pages/api/v1/s-holidays.ts
@@ -0,0 +1,24 @@
+import dayjs from 'dayjs';
+
+import { getHolidays } from 'src/utils/edgeHelpers';
+
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { from, to, only_weekday } = req.query;
+  if (from == null || to == null) {
+    return res.status(500).json({
+      error: 'from, to are required.',
+    });
+  }
+
+  const holidays = getHolidays(
+    dayjs(from as string),
+    dayjs(to as string),
+    only_weekday != null,
+  );
+
+  return res.status(200).json({
+    holidays,
+  });
+}
